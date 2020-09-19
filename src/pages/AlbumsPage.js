9340867fda65d928a0ee349425ca5ce4dbddbb55
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-09-19 12:11:39 -0400

    reconfigured albums page

--- src/pages/AlbumsPage.js
+++ src/pages/AlbumsPage.js
@@ -1,38 +1,53 @@
 import React, { useEffect } from "react";
 import { connect } from 'react-redux';
 import {  Album } from '../components/Album';
-
+import '../App.css';
 //Bring in the Async
 import {fetchAlbums} from '../actions/AlbumsActions'
 
 //Redux state is now in the props of this component
+class AlbumsPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            album: ''
+        };
+    }
 
-const AlbumsPage = ({ dispatch,loading, Album, hasErrors}) => {
-    useEffect(() => {
-        dispatch(fetchAlbums())
-    }, [dispatch])
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.dispatch(fetchAlbums(this.state.album))
+    }
 
-    //show loading, error, or success
+    renderSongs = () => {
+        if (this.props.loading) return <p> Loading Albums</p>
+        if (this.props.hasErrors) return <p> Unable to display Albums.</p>
+        return this.props.albums.map(album => <Album album={album.track}/>)
+    }
 
-    const renderAlbums = () => {
-        if (loading) return <p> Loading Albums</p>
-        if (hasErrors) return <p> Unable to display Albums.</p>
-        console.log(Album)
-        return Album.map(Album => <Album key={Album.id} Album={Album} />)
-    }    
-    return (
-    <section>
-    <div className ="albumspage">
-    <h1 id ="albums">Albums</h1>
-    {renderAlbums()}
-    </div>
-    </section>
-    )
+    render() {
+        return (
+            <>
+                <form onSubmit={e => this.handleSubmit(e)}>
+                    <label>
+                        Album:
+                <input value={this.state.album} onChange={e => this.setState({ album: e.target.value })} type="text" album="album" />
+                    </label>
+                    <button>Submit</button>
+                </form>
+                {this.renderAlbums()}
+            </>
+        );
+
+    }
 }
-const mapStateToProps = (state) => ({
-    loading: state.Albums.loading,
-    Albums: state.Albums.Albums,
-    hasErrors: state.Albums.hasErrors,
-})
+
+const mapStateToProps = state => {
+    return {
+        loading: state.album.loading,
+        hasErrors: state.album.hasErrors,
+        songs: state.album.albums
+    }
+}
 
 export default connect(mapStateToProps)(AlbumsPage);
