1210d9f5ad6e5dcd1a7d0617f7cc2094c4af71d4
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-09-19 11:19:27 -0400

    modified Album component

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,25 +1,31 @@
 
       import React from 'react';
       
-      
-      class Album extends React.Component{
-        render(){
-         const {handleSubmit} = this.props
-         return (
-             <form onSubmit= {handleSubmit}>
+      import React, { useEffect } from "react";
+//Redux state is now in the props of this component
+
+const Album = (props) => {
+    const { album } = props;
+    console.log(album);
+
+    return (
+        <div style={{width: "150px", height: "300px", display: "inline", paddingRight: "30px"}}>
+           
+            <form onSubmit = {handleSubmit}>
                 <label>
-                    Artist:
-                    <input type ="text" album ="album" />
-                    </label> 
-                    <input type ="submit"
-                    value = "Submit" />
-             </form>
-         );
- 
-        }
-            
-             
-       }
-       
+                Artist:
+                    <input type ="text" album ="album"/>
+                    <h1>{album.title}</h1>        
+                    <h2>{album.subtitle}</h2>
+                    <p>Add to Cart<input type ="submit"        
+                     value = "Submit" /></p>
+                </label>
+            </form>
+        </div>
+
+    );
+}
+
+
 
 export default Album;
