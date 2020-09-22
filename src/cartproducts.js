6a6184975071b0a53ce1d2394aa8928f76688936
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-09-22 16:55:46 -0400

    added modal

--- src/cartproducts.js
+++ src/cartproducts.js
@@ -2,6 +2,7 @@ import React, {Component } from 'react'
 import formatCurrency from "../util";
 import Fade from 'react-reveal/Fade';
 import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
 
 export default class cartProducts extends Component {
     constructor(props){
@@ -10,7 +11,14 @@ export default class cartProducts extends Component {
             album:null,
         }
     }
+    openModal =(album) =>{
+        thissetState({album});
+    };
+    closeModal =() =>{
+        this.setState({album:null});
+    }
     render(){
+        const {album} =this.state;
         return(
             <div>
             <Fade bottom cascade>
@@ -18,7 +26,8 @@ export default class cartProducts extends Component {
                    {this.props.albums.map(album =>
                         <li key={album._id}>
                            <div className="album">
-                               <a href={"#" + product._id} onClick={()=>openModal}>
+                               <a href={"#" + product._id} 
+                               onClick={()=>this.openModal(Album)}>
                                 <img src={album.image} alt="album image"></img>
                                 <p>{album.title}</p>
                                 </a>     
@@ -36,6 +45,49 @@ export default class cartProducts extends Component {
                     ))}
                    </ul> 
                    </Fade>
+                   {
+                       album &&
+                       <Modal isOpen={true}>
+                           onRequestClose={this.closeModal}
+                           
+                           <Zoom>
+                               <button className="close-modal" onClick={this.closeModal}>
+                                   x
+                               </button>
+                               <div className ="album-details">
+                                   <img src={album.image} alt ={album.title}></img>
+                                   <div className ="album-details-description">
+                                       <p>
+                                           <strong>{album.title}</strong>
+                                       </p>
+                                       <p>
+                                           {album.description}
+                                       </p>
+                                       <p>
+                                           Genre:{""}
+                                           {album.genre.map(x=>(
+                                               <span> {""}
+                                                <button className="button">{x}</button>
+                                                </span>
+                                           ))}
+                                       </p>
+                                       <div className="album-price">
+                                           <div>{formatCurrency(album.price)}</div>
+                                           <button className="button add" onClick={()=>{
+                                               this.props.addToCart(album)
+                                               this.closeModal();
+                                           }}>
+                                               Add To Cart
+                                               </button>
+                                               
+                                           
+                                       </div>
+                               </div>
+                               </div>
+                            </Zoom>
+                           
+                       </Modal>
+                   }
             </div>
 
         );
